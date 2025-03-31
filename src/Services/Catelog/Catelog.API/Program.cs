6178924cdae8703b8ebf5b5a047f19b67f46fa0c
rykamol <kamolroy.din@gmail.com>
2025-03-31 11:57:49 +0600

    register dependencies and test catelog api

--- src/Services/Catelog/Catelog.API/Program.cs
+++ src/Services/Catelog/Catelog.API/Program.cs
@@ -1,10 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+
+});
 
 
 var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
+
+app.MapCarter();
 
 //Configure the HTTP request pipline
 
