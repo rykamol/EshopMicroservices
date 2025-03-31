fdda2478257f68ce68882f7bf3633b92d0d8c503
rykamol <kamolroy.din@gmail.com>
2025-03-31 11:41:28 +0600

    Complete development of create product endpoint

---
+++ src/Services/Catelog/Catelog.API/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using BuildingBlocks.CQRS;
+global using Carter;
+global using Mapster;
+global using MediatR;
--- src/Services/Catelog/Catelog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catelog/Catelog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,32 @@
 ﻿namespace Catelog.API.Products.CreateProduct
 {
-	public class CreateProductEndpoint
+	public record CreateProductRequest(
+	string name,
+	List<string> category,
+	string description,
+	string imageFile,
+	decimal price);
+
+	public record CreateProductResponse(Guid Guid);
+	public class CreateProductEndpoint : ICarterModule
 	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPost("products/",
+				async (CreateProductRequest request, ISender sender) =>
+			{
+				var command = request.Adapt<CreateProductCommand>();
+
+				var result = await sender.Send(command);
+				var response = result.Adapt<CreateProductResponse>();
+
+				return Results.Created($"/products/{response.Guid}", response);
+			})
+			.WithName("CreateProduct")
+			.Produces<CreateProductResponse>(StatusCodes.Status201Created)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Create Product")
+			.WithDescription("Product created successfully!");
+		}
 	}
 }
