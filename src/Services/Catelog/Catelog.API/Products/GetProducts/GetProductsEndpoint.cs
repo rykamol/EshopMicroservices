a4dd623d8a335edc9c66d3d145d0bfb35b186272
rykamol <kamolroy.din@gmail.com>
2025-04-01 16:22:39 +0600

    Implement getproducs endpoint.

---
+++ src/Services/Catelog/Catelog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catelog.API.Models;
+using Catelog.API.Products.CreateProduct;
+
+namespace Catelog.API.Products.GetProducts
+{
+	public class GetProductsEndpoint : ICarterModule
+	{
+		//public record GetProductsRequest
+		public record GetProductsResponse(IEnumerable<Product> Products);
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("products/",
+				async (ISender sender) =>
+				{
+					var result = await sender.Send(new GetProductsQuery());
+					var response = result.Adapt<GetProductsResponse>();
+
+					return Results.Ok(response);
+				})
+			.WithName("GetProducts")
+			.Produces<CreateProductResponse>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Get Products")
+			.WithDescription("Get Products");
+		}
+	}
+}
---
+++ src/Services/Catelog/Catelog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catelog.API.Models;
+using Marten.Internal;
+using Marten.Linq.QueryHandlers;
+using System.Data.Common;
+using Weasel.Postgresql;
+
+namespace Catelog.API.Products.GetProducts
+{
+	public record GetProductsQuery() : IQuery<GetProductsResult>;
+	public record GetProductsResult(IEnumerable<Product> Products);
+
+	public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+		: IQueryHandler<GetProductsQuery, GetProductsResult>
+	{
+		public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+		{
+			logger.LogInformation("GetProductsQueryHandler.Handle called with {Query}", query);
+
+			var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+			return new GetProductsResult(products);
+		}
+	}
+}
