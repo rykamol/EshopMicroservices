2cd73d14e4d7aea6e7c789e5d96fa0e221b8f72b
rykamol <kamolroy.din@gmail.com>
2025-04-01 22:11:20 +0600

    Implement get product by category endpoint

---
+++ src/Services/Catelog/Catelog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catelog.API.Products.GetProductByCategory
+{
+	public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+	public class GetProductByCategoryEndPoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("products/category/{category}",
+			   async (string category, ISender sender) =>
+			   {
+				   var result = await sender.Send(new GetProductByCategoryQuery(category));
+				   var response = result.Adapt<GetProductByCategoryResponse>();
+				   return Results.Ok(response);
+			   })
+		   .WithName("GetProductByCategory")
+		   .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+		   .ProducesProblem(StatusCodes.Status400BadRequest)
+		   .WithSummary("Get Products by Category")
+		   .WithDescription("Get Products by Category");
+
+		}
+	}
+}
