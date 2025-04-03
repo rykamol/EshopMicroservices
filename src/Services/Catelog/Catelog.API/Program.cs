4ce7bce36531677a72ba291351498ee2754db591
rykamol <kamolroy.din@gmail.com>
2025-04-03 23:35:12 +0600

    Health check catelog api

--- src/Services/Catelog/Catelog.API/Program.cs
+++ src/Services/Catelog/Catelog.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
@@ -20,12 +23,14 @@ builder.Services.AddMarten(opt =>
 
 }).UseLightweightSessions();
 
-if(builder.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 	builder.Services.InitializeMartenWith<CatalogInitialData>();
 
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+	.AddNpgSql(builder.Configuration.GetConnectionString("Database"));
 
 var app = builder.Build();
 //app.MapGet("/", () => "Hello World!");
@@ -62,5 +67,10 @@ app.MapCarter();
 
 app.UseExceptionHandler(opt => { });
 #endregion
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+	ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+}
+ 	);
 
 app.Run();
