110019e7cc23fdccd6cbed07f8c0015d3d0c467e
rykamol <kamolroy.din@gmail.com>
2025-04-05 11:50:05 +0600

    Register carter and mediator

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 //Add services to the container
+var assembly = typeof(Program).Assembly;
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssembly(assembly); //register our mediator into current assembly
+	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+	config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 //Configure the HTTP request
+app.MapCarter();
 
 app.Run();
