efe9176f738943de02db759c4191e3960770329d
rykamol <kamolroy.din@gmail.com>
2025-04-05 11:27:13 +0600

    Implement delete basket feature

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,6 +1,25 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿
+using Basket.API.Basket.StoreBasket;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-	public class DeleteBasketEndpoints
+	//public record DeleteBasketRequest(string UserName);
+	public record DeleteBasketResponse(bool IsSuccess);
+	public class DeleteBasketEndpoints : ICarterModule
 	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/basket/{userName}", async (string userName, ISender sender) =>
+			{
+				var result = await sender.Send(new DeleteBasketCommand(userName));
+				var response = result.Adapt<DeleteBasketResponse>();
+				return Results.Ok(response);
+			})
+			.WithName("DeleteBasket")
+			.Produces<DeleteBasketResponse>(StatusCodes.Status201Created)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Delee basket")
+			.WithDescription("Delete basket");
+		}
 	}
 }
