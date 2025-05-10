6e8ec5a1e7f49d9fa18c90c0c9935a3cbe1882f9
rykamol <kamolroy.din@gmail.com>
2025-05-10 14:58:21 +0600

    Install sql databse and configure the connection.

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+	public class DiscountContext : DbContext
+	{
+		public DiscountContext(DbContextOptions<DiscountContext> options)
+			: base(options)
+		{ }
+
+		public DbSet<Coupon> Coupons { get; set; }
+	}
+}
