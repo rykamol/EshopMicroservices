dfcfddbd7a3210cdfc6adeca581c02079138ee67
rykamol <kamolroy.din@gmail.com>
2025-05-11 12:05:49 +0600

    Register Discount Grpc Client and Discount Grpc Service into the Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Exceptions.Handler;
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using JasperFx;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -6,8 +7,9 @@ using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add services to the container
-var assembly = typeof(Program).Assembly;
 
+//Application Services
+var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
@@ -15,8 +17,9 @@ builder.Services.AddMediatR(config =>
 	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 	config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
-builder.Services.AddValidatorsFromAssembly(assembly);
 
+
+//Data Services
 builder.Services.AddMarten(opt =>
 {
 	opt.Connection(builder.Configuration.GetConnectionString("Database"));
@@ -26,15 +29,26 @@ builder.Services.AddMarten(opt =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CacheBasketRepository>();
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-	options.Configuration = builder.Configuration.GetConnectionString("Redis");
-});
 //builder.Services.AddScoped<IBasketRepository>(provider =>
 //{
 //	var basketRepository = provider.GetRequiredService<BasketRepository>(); ;
 //	return new CacheBasketRepository(basketRepository,provider.GetRequiredService<IDistributedCache>());
 //});
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+	options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+	options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]);
+});
+
+
+//Cross Cutting Services
+builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
@@ -43,7 +57,12 @@ builder.Services.AddHealthChecks()
 	.AddNpgSql(builder.Configuration.GetConnectionString("Database"))
 	.AddRedis(builder.Configuration.GetConnectionString("Redis"));
 
+
+
+
 var app = builder.Build();
+
+
 //Configure the HTTP request
 app.MapCarter();
 app.UseExceptionHandler(e => { });
