9e0ff19769dd3003e745e5e63472c87beb9c6cea
rykamol <kamolroy.din@gmail.com>
2025-05-17 15:19:21 +0600

    Domain Modeling with DDD for Ordering Microservice

--- src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Ordering.Domain.Abstractions
 {
 	public abstract class Entity<T> : IEntity<T>
--- src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -2,9 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Ordering.Domain.Abstractions
 {
 	public interface IDomainEvent : INotification //Inotification will ditchpatch with mediatr handler
