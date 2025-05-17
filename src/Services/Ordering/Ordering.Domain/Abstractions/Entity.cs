06549eb6a0ecb7cedb19026644f8574d1aa7c9f1
rykamol <kamolroy.din@gmail.com>
2025-05-17 15:56:00 +0600

    DDD Rich-domain model Entity for Order Entities

--- src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-namespace Ordering.Domain.Abstractions
+﻿namespace Ordering.Domain.Abstractions
 {
 	public abstract class Entity<T> : IEntity<T>
 	{
--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -2,6 +2,19 @@
 {
 	public record CustomerId
 	{
-		public Guid Id { get; }
-	}
+		private Guid Value;
+
+        public CustomerId(Guid value) => Value = value; 
+        public static CustomerId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if(value ==Guid.Empty)
+            {
+                throw new DomainException("Customer Id cannot be empty.");
+            }
+
+            return new CustomerId(value);
+        }
+    }
 }
