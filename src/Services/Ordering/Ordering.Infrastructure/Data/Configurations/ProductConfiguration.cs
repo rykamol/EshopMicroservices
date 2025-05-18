bfe87806724863f40502a6fb11bcaa20e38c4b3e
rykamol <kamolroy.din@gmail.com>
2025-05-18 18:09:58 +0600

    Product Configuration

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+	internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.HasKey(c => c.Id);
+			builder.Property(c => c.Id).HasConversion(productId => productId.Value, dbid => ProductId.Of(dbid));
+
+			builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
+		}
+	}
+}
