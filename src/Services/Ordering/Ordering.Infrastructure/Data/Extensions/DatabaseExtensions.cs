fdb00d96d756745b350635b8f63a1e31915bbdbb
rykamol <kamolroy.din@gmail.com>
2025-05-24 23:21:39 +0600

    Seed Customer Data

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -11,6 +11,22 @@ namespace Ordering.Infrastructure.Data.Extensions
 
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+            await SeedAsync(context);
+        }
+
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomerAsyc(context);
+        }
+
+        private static async Task SeedCustomerAsyc(ApplicationDbContext context)
+        {
+            if (!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Infrastructure.Data.Extensions
+{
+    internal class InitialData
+    {
+        public static IEnumerable<Customer> Customers = new List<Customer>()
+            {
+                Customer.Create(CustomerId.Of(new Guid("34ce2bf5-bd3e-4441-ad28-7ace7c85ae28")),"Kamol","kamol@sample.com"),
+                Customer.Create(CustomerId.Of(new Guid("827a933a-6eed-4914-9bd7-2d165efbadf2")),"Roy","roy@sample.com")
+            };
+    }
+}
